0ad4ee74e845477ee97dfd83f08d9389dbf9bb55
IntuitionAmiga <intuitionamiga@gmail.com>
2018-07-14 17:07:03 +0000

    First commit

---
+++ reboot.cpp
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2018, Zayn Otley.
+ * Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ *      Zayn Otley (zayn@certificationcourses.co.uk)
+ */
+
+/*!	Reboots the system. */
+
+/* Build with:
+ *
+ * g++ -Ofast -march=native \
+ * -I/boot/system/develop/headers/private/system/ \
+ * -I/boot/home/src/haiku/headers/private/system/arch/x86_64 \
+ * reboot.cpp -o reboot -lbe
+ *
+ */
+
+#include "/boot/system/develop/headers/private/system/syscalls.h"
+
+int main(int argc, char **argv) {
+    _kern_shutdown(true);
+    return 0;
+}
+
